a5368a9f5b75a445bd0f6a6d3a38b0c5c98ade2a
nearyou777 <v.pelishenko1999@gmail.com>
2024-06-04 23:12:12 +0300

    minor bug fixes

--- delete_offrers.py
+++ delete_offrers.py
@@ -1,7 +1,7 @@
 import telebot
 from time import sleep
 from telebot import types
-from config import Tickets,Users, SentMessage
+from models import Tickets,Users, SentMessage
 from sqlalchemy.orm import sessionmaker
 import math
 from config import engine
@@ -10,7 +10,7 @@ import os
 import json
 from config import all_airports
 from datetime import datetime, timedelta
-from config import Tickets, engine
+
 load_dotenv()
 
 Session = sessionmaker(bind=engine)
