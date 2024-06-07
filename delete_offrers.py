86470b93a301da0602c5ac053c977f2309045f8e
nearyou777 <v.pelishenko1999@gmail.com>
2024-06-08 00:42:24 +0300

    database optimisation, minor bug fix

--- delete_offrers.py
+++ delete_offrers.py
@@ -10,13 +10,22 @@ import json
 from config import all_airports
 from datetime import datetime, timedelta
 from sqlalchemy.orm import sessionmaker
+from bot import bot
 load_dotenv()
 Session = sessionmaker(bind=engine)
 
 def autodelete():
     with Session() as session:
-        session.query(Tickets).filter(Tickets.DateAdded >= Tickets.DateAdded + timedelta(days=30)).delete(synchronize_session=False)
+        data = session.query(Tickets).filter(Tickets.DateAdded >= Tickets.DateAdded + timedelta(days=30)).all()
+        for row in data:
+            if os.path.isfile(f'/imgs/{row.PictureName}'):
+                try:
+                    os.remove(f'/imgs/{row.PictureName}')
+                except Exception as e:
+                    bot.send_message(os.getenv('my_id'), f'Error while deleting file\n{e}')
+        data = session.query(Tickets).filter(Tickets.DateAdded >= Tickets.DateAdded + timedelta(days=30)).delete(synchronize_session=False)
         session.commit()
+       
 
 
 if __name__ == '__main__':
